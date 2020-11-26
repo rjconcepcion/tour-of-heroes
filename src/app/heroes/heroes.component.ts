b409d9dc31fc0dc717a16c1c430346e8cec4dce5
Robert Concepcion <robertjohnconcepcion@gmail.com>
2020-11-26 23:32:17 +0800

    add hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,6 +11,12 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
   
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
